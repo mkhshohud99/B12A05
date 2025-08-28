b1e5dfe5cfa928e179a11e289e6fc08de1833af5
mkhshohud99 <mkhshohud1999@gmail.com>
2025-08-29 03:23:42 +0600

    Main caiteria is done

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
 const transactionData = []
-const coin = getInnerText("coin")
-function getInnerText(id) {
+
+// History Function
+function setHistory() {
+    const historyContainer = document.getElementById("history-container")
+    historyContainer.innerText = ""
+    for (const data of transactionData) {
+        const div = document.createElement("div")
+        div.innerHTML = `
+        <div class="flex items-center justify-between mt-5 p-2 bg-gray-100 rounded-lg">
+                    <div>
+                        <h2 class="font-semibold text-lg">${data.name}</h2>
+                        <p>${data.number}</p>
+                    </div>
+                    <div>
+                        <p>${data.date}</p>
+                    </div>
+                </div>
+        `
+        historyContainer.appendChild(div)
+    }
+}
+function getInnerTextValue(id) {
     const innerNumber = parseInt(document.getElementById(id).innerText)
     return innerNumber
+}
+function getInnerText(id) {
+    const innerText = document.getElementById(id).innerText
+    return innerText
 }
 
 function setInnerText(value) {
     document.getElementById("coin").innerText = value
 }
+// Hart Function
+let hearts = document.getElementsByClassName("fa-heart")
+for (const heart of hearts) {
+    heart.addEventListener("click", function (e) {
+        e.preventDefault()
+        const heartReact = getInnerTextValue("heart-count")
+        totalReacts = heartReact + 1
+        document.getElementById("heart-count").innerText = totalReacts
+    }
+    )
+}
+
+// Copy Count
+let copys = document.getElementsByClassName("copy")
+for (const copy of copys) {
+    copy.addEventListener("click", function (e) {
+        e.preventDefault()
+        const copyCount = getInnerTextValue("copy-count")
+        totalCopys = copyCount + 1
+        document.getElementById("copy-count").innerText = totalCopys
+    }
+    )}
+
+//Clear Section
+document.getElementById("clear-btn").addEventListener("click", function () {
+    const historyContainer = document.getElementById("history-container")
+    historyContainer.innerText = ""
+    transactionData.length = 0
+})
 
 // Call Section
 // For Emergency Section
 document.getElementById("emergency-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -28,13 +81,16 @@ document.getElementById("emergency-btn").addEventListener("click", function (e) 
         date: new Date().toLocaleTimeString()
     }
 
+
     transactionData.push(data)
+    setHistory()
+
 })
 
 // For Police
 document.getElementById("police-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -50,12 +106,13 @@ document.getElementById("police-btn").addEventListener("click", function (e) {
     }
 
     transactionData.push(data)
+    setHistory()
 })
 
 // Fire Service
 document.getElementById("fire-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -71,11 +128,12 @@ document.getElementById("fire-btn").addEventListener("click", function (e) {
     }
 
     transactionData.push(data)
+    setHistory()
 })
 // Fire Ambulance Service
 document.getElementById("ab-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -91,11 +149,12 @@ document.getElementById("ab-btn").addEventListener("click", function (e) {
     }
 
     transactionData.push(data)
+    setHistory()
 })
 // Fire Women & Child Helpline
 document.getElementById("wc-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -111,11 +170,12 @@ document.getElementById("wc-btn").addEventListener("click", function (e) {
     }
 
     transactionData.push(data)
+    setHistory()
 })
 // Anti-Corruption Helpline
 document.getElementById("ac-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -131,11 +191,12 @@ document.getElementById("ac-btn").addEventListener("click", function (e) {
     }
 
     transactionData.push(data)
+    setHistory()
 })
 // Electricity Helpline
 document.getElementById("eh-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -151,11 +212,12 @@ document.getElementById("eh-btn").addEventListener("click", function (e) {
     }
 
     transactionData.push(data)
+    setHistory()
 })
 // Brac Helpline
 document.getElementById("bc-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -171,11 +233,12 @@ document.getElementById("bc-btn").addEventListener("click", function (e) {
     }
 
     transactionData.push(data)
+    setHistory()
 })
 // Bangladesh Railway Helpline
 document.getElementById("br-btn").addEventListener("click", function (e) {
     e.preventDefault()
-
+    const coin = getInnerTextValue("coin")
     if (coin <= 0) {
         alert("You have at last 20 coin for call")
         return;
@@ -191,4 +254,9 @@ document.getElementById("br-btn").addEventListener("click", function (e) {
     }
 
     transactionData.push(data)
+    setHistory()
 })
+
+
+
+
